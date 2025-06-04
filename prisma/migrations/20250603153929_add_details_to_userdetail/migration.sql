67a13fb5e0fe5003c40c022c2d8e8cb3c6a565fa
elidayjuma <elidejuma@gmail.com>
2025-06-04 08:06:26 +0300

    fix migrations issue

--- prisma/migrations/20250603153929_add_details_to_userdetail/migration.sql
+++ prisma/migrations/20250603153929_add_details_to_userdetail/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE `userdetail` ADD COLUMN `careerTitle` VARCHAR(191) NULL,
+ALTER TABLE `UserDetail` ADD COLUMN `careerTitle` VARCHAR(191) NULL,
     ADD COLUMN `githubUrl` VARCHAR(191) NULL,
     ADD COLUMN `portfolioUrl` VARCHAR(191) NULL,
     ADD COLUMN `twitterUrl` VARCHAR(191) NULL;
