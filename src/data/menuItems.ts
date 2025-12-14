983b34cfdf858063bc066d11c9965195b3d42608
elidayjuma <elidejuma@gmail.com>
2025-12-14 21:30:03 +0300

    remove pricing

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -5,6 +5,6 @@ export const menuItems: IMenuItem[] = [
     { text: "Create Cover Letter", url: "/coverletter" },
     { text: "Tailor my CV", url: "/Cv" },
     { text: "Interview Prep", url: "/InterviewPrep" },
-    { text: "Pricing", url: "/#pricing" },
+    // { text: "Pricing", url: "/#pricing" },
     // { text: "By ElidayJuma.com", url: "https://elidayjuma.com" },
 ];
